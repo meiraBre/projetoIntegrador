1a5c08b0ffdf6f35da9f4be3d43e6a60fb451c9b
Alicia Paula Coimbra <160132305+aliciapaula@users.noreply.github.com>
2025-03-27 21:05:41 -0300

    atualização das páginas de login e cadastro

--- global.js
+++ global.js
@@ -5,7 +5,17 @@ class Header extends HTMLElement{
                 <div class="logo-container">
                     <img src="assets/doce_encanto_logo_circular.png" alt="Logo">
                 </div>
+                <nav class="nav-menu">
+                    <a href="index.html">Início</a>
+                    <a href="#">Produtos</a>
+                    <a href="apresentaçao.html">Sobre</a>
+                </nav>
+                <div class="icons-container">
+                    <img src="assets/icone carrinho.png" alt="Carrinho" width="25px">
+                    <a href="login.html"><img src="assets/icone usuário.png" alt="Usuário" width="25px"></a>
+                </div>
             </header>
+            
         `
     }
 }
