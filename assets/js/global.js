410d5a75e812cd3c7e97d39dc61476de16959db8
Alicia Paula <aliciapaula0001@gmail.com>
2025-04-16 14:33:30 -0300

    criação do menu hamburguer

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,26 +1,31 @@
 class Header extends HTMLElement{
     connectedCallback(){
         this.innerHTML = `
-<header>
-    <div class="logo">
-        <img src="./assets/img/doce_encanto_logo_circular.png" alt="Logo da loja de doces" width="60px">
-    </div>
-    <div class="lista">
-        <ul>
-            <li><a href="./index.html"><button>Início</button></a></li>
-            <li><a href="./comprar.html"><button>Produtos</button></a></li>
-            <li><a href="./apresentaçao.html"><button>Sobre</button></a></li>
-        </ul>
-    </div>
-    <div class="icones">
-        <a href="Rastreamento.html">
-            <img src="./assets/img/icone-carrinho.png" alt="Carrinho" width="25px">
-        </a>
-        <a href="login.html">
-            <img src="./assets/img/icone-usuário.png" alt="Usuário" width="25px">
-        </a>
-    </div>
-</header>
+            <header>
+                <div class="logo">
+                    <img src="./assets/img/doce_encanto_logo_circular.png" alt="Logo da loja de doces" width="60px">
+                </div>
+                <div class="lista">
+                    <ul class="navMenu">
+                        <li><a href="./index.html"><button>Início</button></a></li>
+                        <li><a href="./comprar.html"><button>Produtos</button></a></li>
+                        <li><a href="./apresentaçao.html"><button>Sobre</button></a></li>
+                    </ul>
+                </div>
+                <div class="icones">
+                    <a href="Rastreamento.html">
+                        <img src="./assets/img/icone-carrinho.png" alt="Carrinho" width="25px">
+                    </a>
+                    <a href="login.html">
+                        <img src="./assets/img/icone-usuário.png" alt="Usuário" width="25px">
+                    </a>
+                </div>
+                <div class="hamburger">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </div>
+            </header>
         `
     }
 }
