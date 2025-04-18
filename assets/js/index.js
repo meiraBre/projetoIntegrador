ce37626d7ee19c25810601ba4098c9142df1771c
Brenda <bremeirah@gmail.com>
2025-04-18 15:08:46 -0300

    Atualização barra de pesquisa

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,23 @@
 const btnLupa = document.getElementById('btn-lupa');
 const barraPesquisa = document.getElementById('barra-pesquisa');
+const inputPesquisa = document.getElementById('pesquisa');
 
 btnLupa.addEventListener('click', () => {
   barraPesquisa.classList.toggle('escondido');
 });
+
+inputPesquisa.addEventListener('input', () => {
+  const termo = inputPesquisa.value.toLowerCase();
+  const produtos = document.querySelectorAll('.produto');
+
+  produtos.forEach(produto => {
+    const nome = produto.querySelector('.nomeProduto').textContent.toLowerCase();
+    if (nome.includes(termo)) {
+      produto.style.display = 'inline-block';
+    } else {
+      produto.style.display = 'none';
+    }
+  });
+});
+
+  
